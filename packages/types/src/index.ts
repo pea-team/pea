914a7684587839a3754ceb235a37c5a0f776e50e
forsigner <forsigner@gmail.com>
2019-10-11 21:21:47 +0800

    feat: create package types

---
+++ packages/types/src/index.ts
@@ -0,0 +1,16 @@
+import { Configuration } from 'webpack'
+export interface IPlugin {
+  beforeCompile?: () => any
+  updateWebpackConfig?: (config: Configuration) => Configuration
+  addImportCode?: () => any
+  addBootstrapCode?: () => any
+  addRenderCode?: () => any
+}
+
+export interface PeaConfig {
+  title?: string
+  appHtml?: string
+  outputHtml?: string
+  buildDir?: string
+  plugins: IPlugin[]
+}
